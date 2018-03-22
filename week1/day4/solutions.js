6ca9069f27f55b036dfbba436e1bd079c7d0af90
jealvarez123 <jealvarez123@gmail.com>
2018-03-22 18:26:25 -0500

    Commit 1 - Verbal questions

---
+++ week1/day4/solutions.js
@@ -0,0 +1,10 @@
+Functions:
+
+// Parameter vs arugment
+// Parameter work as placeholders in the () of the function
+// arugments are the items you plug into the placeholder that is outside of the function
+
+// Return will stop the function and return the specific value
+//
+// console log will display information in console
+//

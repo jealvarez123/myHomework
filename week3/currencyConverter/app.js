0706653007075494e6d3972330cce9b471224966
jealvarez123 <jealvarez123@gmail.com>
2018-04-04 08:57:36 -0500

    added the ajax call and put in some work on getting the input converted

--- week3/currencyConverter/app.js
+++ week3/currencyConverter/app.js
@@ -1,1 +1,40 @@
 console.log('Can I kick it?');
+
+
+
+$.ajax({
+  // What kind of request
+  method: 'GET',
+
+  // The URL for the request
+  url: 'http://data.fixer.io/api/latest?access_key=37922329f842dea02c0bae4788509542&symbols=USD,EUR,CNY,GBP,JPY',
+
+  // The type of data we want back
+  dataType: 'json',
+
+  // Code to run if the request succeeds; the JSON
+  // response is passed to the function as an argument.
+  success: onSuccess
+});
+
+// defining the callback function that will happen
+// if the request succeeds.
+function onSuccess(responseData) {
+var rates = responseData.rates
+console.log(rates);
+    // console.log(responseData.rates);
+    // celebrate!
+};
+
+function convertCurrency(event) {
+  if ('type' in event.target && event.target.type === 'button') {
+  let convert = responseData.rates * 'money';
+    return convert;
+    console.log('convert');
+  }if ('type' in event.target && event.target.type === 'button') {
+    console.log('handle currency conversion here');
+  }
+}
+
+document.body.addEventListener('click', convertCurrency);
+  console.log('Click!');
